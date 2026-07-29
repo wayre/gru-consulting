0de22930e4a3bc2ab0a58b94f7dcd9a8200fcb21
Wayre Avelar <wayre1@gmail.com>
2026-07-29 18:10:39 -0300

    feat: ajuste imagens

--- components/sections/Clientes.tsx
+++ components/sections/Clientes.tsx
@@ -8,15 +8,15 @@ export const Clientes = () => {
 
   // Array com as marcas/clientes parceiros para facilidade de manutenção
   const clientLogos = [
-    { name: "Multiparts", src: "logo-multiparts.png" },
-    { name: "Grupo Maxi", src: "logo-grupo-maxi.png" },
-    // { name: "Tropical Estufas", src: "logo-tropical-estufas.png" },
-    { name: "Valflex", src: "logo-valflex.png" },
-    { name: "Rocha Bahia", src: "logo-rocha-bahia.png" },
-    { name: "Tetralite", src: "logo-tetralite.png" },
-    { name: "Motiv", src: "logo-motiv.png" },
-    { name: "Fortuna", src: "logo-fortuna.png" },
-    { name: "Renz Intrumentos Elétricos", src: "logo-renz-intrumentos-eletricos.png" }
+    { name: "Multiparts", src: "multiparts1.webp" },
+    { name: "Grupo Maxi", src: "grupo-maxi-preto.webp" },
+    { name: "Tropical Estufas", src: "tropical-estufas.webp" },
+    { name: "Valflex", src: "valflexBlack.webp" },
+    { name: "Rocha Bahia", src: "rocha-bahia1.webp" },
+    { name: "Tetralite", src: "tetralite.webp" },
+    { name: "Motiv", src: "motiv1.webp" },
+    { name: "Fortuna", src: "fortuna.webp" },
+    { name: "Renz Intrumentos Elétricos", src: "renz-intrumentos-eletricos.webp" }
   ];
 
   // Cria uma lista de logos rotacionada a partir do índice 4 para a segunda animação
@@ -31,7 +31,7 @@ export const Clientes = () => {
       <p className="text-center text-xl font-semibold text-black/70 my-10">Nossos Clientes Parceiros</p>
       {/* Segunda linha animada: move-se suavemente para a direita */}
       <div className="w-5/6 m-auto overflow-hidden">
-        <div className="flex w-max animate-marquee-right py-2 bg-black rounded-4xl border border-zinc-800/10">
+        <div className="flex w-max animate-marquee-right py-2 bg-black/5 rounded-4xl border border-zinc-800/10">
           {/* Primeiro set de logos */}
           {rotatedLogos.map((logo, index) => (
             <div
@@ -54,7 +54,7 @@ export const Clientes = () => {
               className="flex h-10 w-24 items-center justify-center opacity-100 transition-all duration-300 hover:grayscale-0 hover:opacity-100 sm:w-28 md:w-32 flex-shrink-0 mx-4 sm:mx-6 md:mx-8"
             >
               <Image
-                src={"/logo-empresas/" + logo.src}
+                src={"/logos-empresas/" + logo.src}
                 alt={`Logo da marca ${logo.name}`}
                 width={150}
                 height={50}
